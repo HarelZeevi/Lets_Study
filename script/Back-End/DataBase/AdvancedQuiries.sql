eadb7bdef1e703f3dd4ab686638d9780fd8db83a
Harel Zeevi <62669852+HarelZeevi@users.noreply.github.com>
2021-05-18 20:26:06 +0300

    Update AdvancedQuiries.sql

--- script/Back-End/DataBase/AdvancedQuiries.sql
+++ script/Back-End/DataBase/AdvancedQuiries.sql
@@ -2,4 +2,7 @@
 # method: look for a tutor
 SELECT id, firstName, lastName 
 FROM students
-WHERE grade= /*grade*/
+WHERE grade = /*grade*/
+INNER JOIN students ON students.id = turtors.id
+LEFT JOIN ;
+      
